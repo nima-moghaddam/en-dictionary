cf7b08c2d88cf3ec871cd3267df1ac776fabad8b
Nima <nima.73sm@yahoo.com>
2022-12-29 01:35:38 +0330

    installed react-toggle and utillized it

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,23 +3,36 @@ import { ThemeContext } from "../../context/ThemeContext";
 import moonSvg from "./../../assets/images/moon.svg";
 import sunSvg from "./../../assets/images/sun.svg";
 import bookSvg from "./../../assets/images/book.svg"
-import "./Navbar.scss";
 import { motion } from 'framer-motion';
+import Toggle from 'react-toggle'
+import "./Navbar.scss";
+import "react-toggle/style.css"
+
 
 const Navbar = () => {
   const { theme, setTheme } = useContext(ThemeContext);
 
   return (
     <header className='navbar' >
-      <span className="navbar__tag">
+      <div className="navbar__tag">
         <span className="navbar__tag-icon">
           <img src={bookSvg} alt='book' />
         </span>
         <span>English Dictionary</span>
-      </span>
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")} className={`navbar__toggle-btn`}>
+      </div>
+      <label>
+        <Toggle
+          defaultChecked={false}
+          onChange={() => setTheme(theme === "light" ? "dark" : "light")} 
+          icons={{
+            checked: <img className="navbar__toggle-icon" src={moonSvg} />,
+            unchecked: <img className="navbar__toggle-icon" src={sunSvg} />,
+          }}
+        />       
+      </label>
+      {/* <button onClick={() => setTheme(theme === "light" ? "dark" : "light")} className={`navbar__toggle-btn`}>
         <motion.img  src={theme === "light" ? moonSvg : sunSvg} alt='toggle'/>
-      </button>
+      </button> */}
     </header>
   );
 };
